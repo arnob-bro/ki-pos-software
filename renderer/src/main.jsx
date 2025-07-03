7d807529fb6f356d42a3b1b759f1293052dd7cdf
arnob-bro <abdulmohaimenkhanarnob@gmail.com>
2025-07-04 03:36:14 +0600

    ....

--- renderer/src/main.jsx
+++ renderer/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HashRouter} from 'react-router-dom'
 import './index.css'
@@ -8,9 +7,4 @@ createRoot(document.getElementById("root")).render(
 	<HashRouter>
 		<App />
 	</HashRouter>
-);
-createRoot(document.getElementById("root")).render(
-	<Router>
-		<App />
-	</Router>
 );
