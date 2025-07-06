8aaeba7cd0c526c7d2e22134e342898014c1d583
arnob-bro <abdulmohaimenkhanarnob@gmail.com>
2025-07-06 22:26:44 +0600

    edited sidebar and resolves conflicts - arnob

--- renderer/src/components/Sidebar.jsx
+++ renderer/src/components/Sidebar.jsx
@@ -44,17 +44,18 @@ const Sidebar = () => {
       >
         📄 Product Management
       </button>
-      <button style={{marginTop: 'auto',backgroundColor: '#dc3545',color: '#fff',border: 'none',padding: '5px',
-          borderRadius: '6px',cursor: 'pointer',fontWeight: 'bold',transition: 'background-color 0.3s ease',}}
-          onClick={handleLogout}onMouseEnter={e => e.target.style.backgroundColor = '#c82333'}
-          onMouseLeave={e => e.target.style.backgroundColor = '#dc3545'} >Logout
-      </button>
       <button
         className={`nav-btn${location.pathname === '/employee-management' ? ' active' : ''}`}
         onClick={() => navigate('/employee-management')}
       >
         📄 Employee Management
       </button>
+      <button style={{marginTop: 'auto',backgroundColor: '#dc3545',color: '#fff',border: 'none',padding: '5px',
+          borderRadius: '6px',cursor: 'pointer',fontWeight: 'bold',transition: 'background-color 0.3s ease',}}
+          onClick={handleLogout}onMouseEnter={e => e.target.style.backgroundColor = '#c82333'}
+          onMouseLeave={e => e.target.style.backgroundColor = '#dc3545'} >Logout
+      </button>
+      
     </aside>
   );
 };
