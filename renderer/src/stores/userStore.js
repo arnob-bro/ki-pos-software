6054eab7b0edbec769b72525f075c7cc697a49d9
arnob-bro <abdulmohaimenkhanarnob@gmail.com>
2025-07-11 02:32:37 +0600

    ...

--- renderer/src/stores/userStore.js
+++ renderer/src/stores/userStore.js
@@ -59,6 +59,7 @@ const useUserStore = create(
               isLoading: false,
               error: null
             });
+            console.log(result);
             return { success: true, user: result.user };
           } else {
             set({
